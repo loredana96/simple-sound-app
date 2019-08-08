c56f0ef1517f6fb7fb2d18bd628966c0b75304dd
loredanatodoran <loredanatodoran15@gmail.com>
2019-08-08 12:22:33 +0300

    Initial commit.

---
+++ MusicApp/MainScreenViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainScreenViewController.swift
+//  MusicApp
+//
+//  Created by Loredana Todoran on 07/08/2019.
+//  Copyright © 2019 Loredana Todoran. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+   
+    
+    @IBOutlet weak var Geography: UIButton!
+    @IBOutlet weak var Math: UIButton!
+    @IBOutlet weak var Music: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+   
+}
+
